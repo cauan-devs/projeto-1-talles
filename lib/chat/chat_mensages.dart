88bce55d148e4033c27dcfa219f8050c669e2c88
cauan <cauan123silvasantos@gmail.com>
2026-06-19 18:24:02 -0300

    scroll dentro das conversas (chat_mensages) e ajustando um pouco a posição das mensagens.

--- lib/chat/chat_mensages.dart
+++ lib/chat/chat_mensages.dart
@@ -24,12 +24,13 @@ class _MensagensState extends State<Mensagens> {
         backgroundColor: Colors.lightGreen,
       ),
       backgroundColor: Colors.green,
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
         children: [
           Container(
             width: 300,
             height: 100,
-            margin: EdgeInsets.only(left: 0, bottom: 100, top: 60, right: 150),
+            margin: EdgeInsets.only(left: 0, bottom: 100, top: 30, right: 180),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12.0),
               color: Colors.greenAccent
@@ -63,9 +64,9 @@ class _MensagensState extends State<Mensagens> {
             ),
             ),
          Container(
-             width: 100,
-            height: 100,
-            margin: EdgeInsetsDirectional.only(end: 360, top: 50),
+             width: 150,
+            height: 80,
+            margin: EdgeInsetsDirectional.only(end: 330, top: 50),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12.0),
               color: Colors.greenAccent
@@ -74,9 +75,9 @@ class _MensagensState extends State<Mensagens> {
               "hahaha não. Vai trabalhar"),
          ),
            Container(
-                width: 100,
-               height: 80,
-            margin: EdgeInsetsDirectional.only(start: 385, top: 50),
+                width: 80,
+               height: 65,
+            margin: EdgeInsetsDirectional.only(start: 410, top: 50),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12.0),
               color: Colors.indigoAccent),
@@ -84,6 +85,28 @@ class _MensagensState extends State<Mensagens> {
             child: Text(
               ":(")
            ),
+            Container(
+             width: 250,
+            height: 70,
+            margin: EdgeInsetsDirectional.only(end: 340, top: 50, start: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.greenAccent
+            ),padding: EdgeInsets.all(10),
+            child: Text(
+              "amanhã tu vem pra nois assistir a copa"),
+            ),
+             Container(
+                width: 200,
+               height: 80,
+            margin: EdgeInsetsDirectional.only(start: 290, top: 50),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.indigoAccent),
+              padding: EdgeInsets.all(20),
+            child: Text(
+              "agora vi vantagem, chefia"),
+             ),
            Container(
                 width: 550,
                height: 60,
@@ -102,7 +125,8 @@ class _MensagensState extends State<Mensagens> {
             ),
            ),
         ],
-      )
+      ),
+      ),
     );
   }
 }
